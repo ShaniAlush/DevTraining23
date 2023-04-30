bd9a69f8b759e9687d6a5cff2f9b17f1e59fb0c1
shani alush <shani9901@gmail.com>
2023-04-30 12:57:14 +0300

    transition + starting bonus

--- pages/day1b.jsx
+++ pages/day1b.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import styles from './day1b.module.css';
 
 export default function Day1b() {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.navBar} />
       <h1>Day 1 Bonus Challenge</h1>
     </div>
   );
